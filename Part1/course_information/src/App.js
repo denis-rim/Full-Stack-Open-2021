f815a2f7827b127bc4f8b7620b15ff8ff6a9d027
denis-rim <denis.aurorim@gmail.com>
2021-04-05 14:43:20 +0300

    1.1: course information, step1

--- Part1/course_information/src/App.js
+++ Part1/course_information/src/App.js
@@ -1,29 +1,61 @@
-import React from 'react'
+import React from "react";
+
+const Header = ({ title }) => {
+  return <h1>{title}</h1>;
+};
+
+const Content = ({
+  part1,
+  part2,
+  part3,
+  exercises1,
+  exercises2,
+  exercises3,
+}) => {
+  return (
+    <div>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+    </div>
+  );
+};
+
+const Total = ({ total }) => {
+  return <p>Number of exercises: {total}</p>;
+};
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  const total = exercises1 + exercises2 + exercises3;
 
   return (
-      <div>
-        <h1>{course}</h1>
-        <p>
-          {part1} {exercises1}
-        </p>
-        <p>
-          {part2} {exercises2}
-        </p>
-        <p>
-          {part3} {exercises3}
-        </p>
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-      </div>
-  )
-}
+    <div>
+      <Header title={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total total={total} />
+    </div>
+  );
+};
 
-export default App
+export default App;

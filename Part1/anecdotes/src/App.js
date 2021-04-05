f9caf6f5a3e464c1d4491cab1da3c7b6c7a780eb
denis-rim <denis.aurorim@gmail.com>
2021-04-05 17:31:23 +0300

    1.12*: anecdotes step1

--- Part1/anecdotes/src/App.js
+++ Part1/anecdotes/src/App.js
@@ -1,18 +1,34 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const anecdotes = [
-    "If it hurts, do it more often",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-  ];
-
   const [selected, setSelected] = useState(0);
 
-  return <div>{anecdotes[selected]}</div>;
+  const getRandomNumber = () => {
+    const newSelected = Math.round(Math.random() * (anecdotes.length - 1));
+    if (newSelected !== selected) {
+      setSelected(newSelected);
+      return;
+    }
+    getRandomNumber();
+  };
+
+  return (
+    <div className="container">
+      <div className="anecdote-container">{anecdotes[selected]}</div>
+      <div className="button-container">
+        <button onClick={getRandomNumber}>Next Anecdote</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
+
+const anecdotes = [
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+];

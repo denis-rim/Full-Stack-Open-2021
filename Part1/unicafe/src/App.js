87b950ec38a8ffb7b331a0f7e05826cf0f8ce92a
denis-rim <denis.aurorim@gmail.com>
2021-04-05 16:24:31 +0300

    1.6: unicafe step1

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 function App() {
-  return <div>unicafe</div>;
+  const [goodVote, setGoodVote] = useState(0);
+  const [neutralVote, setNeutralVote] = useState(0);
+  const [badVote, setBadVote] = useState(0);
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <div>
+        <button onClick={() => setGoodVote(goodVote + 1)}>Good</button>
+        <button onClick={() => setNeutralVote(neutralVote + 1)}>Neutral</button>
+        <button onClick={() => setBadVote(badVote + 1)}>Bad</button>
+      </div>
+      <div>
+        <h2>Statistics:</h2>
+        <p>Good: {goodVote}</p>
+        <p>Neutral: {neutralVote}</p>
+        <p>Bad: {badVote}</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;

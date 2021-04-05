788c791d23fc7fa053a52f14c39ee8ef4fd2115d
denis-rim <denis.aurorim@gmail.com>
2021-04-05 21:12:18 +0300

    2.4: Course information step9

--- Part2/course_information/src/App.js
+++ Part2/course_information/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = ({ name }) => {
-  return <h1>{name}</h1>;
+  return <h2>{name}</h2>;
 };
 
 const Part = ({ name, exercises }) => {
@@ -36,6 +36,7 @@ const Total = ({ parts }) => {
 const Course = ({ course }) => {
   return (
     <div>
+      <h1>Web development curriculum</h1>
       <Header name={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
@@ -44,29 +45,58 @@ const Course = ({ course }) => {
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return (
+    <div>
+      {courses.map((course) => {
+        return <Course course={course} />;
+      })}
+    </div>
+  );
 };
 
 export default App;

d665a0dbf15d7d9ace6e72333baafc920b784f02
denis-rim <denis.aurorim@gmail.com>
2021-04-05 15:49:45 +0300

    1.5: course information step5

--- Part1/course_information/src/App.js
+++ Part1/course_information/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Header = ({ course }) => {
-  return <h1>{course}</h1>;
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
 };
 
 const Part = ({ name, exercises }) => {
@@ -32,27 +32,29 @@ const Total = ({ parts }) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };

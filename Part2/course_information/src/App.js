83c9a05a74370e7baf63d5a2b830c18993876ea2
denis-rim <denis.aurorim@gmail.com>
2021-04-05 20:55:35 +0300

    2.1: Course information step6

--- Part2/course_information/src/App.js
+++ Part2/course_information/src/App.js
@@ -13,11 +13,14 @@ const Part = ({ name, exercises }) => {
 };
 
 const Content = ({ parts }) => {
+  console.log(parts);
   return (
     <div>
-      <Part name={parts[0].name} exercises={parts[0].exercises} />
-      <Part name={parts[1].name} exercises={parts[1].exercises} />
-      <Part name={parts[2].name} exercises={parts[2].exercises} />
+      {parts.map((part) => {
+        return (
+          <Part key={part.id} name={part.name} exercises={part.exercises} />
+        );
+      })}
     </div>
   );
 };
@@ -31,32 +34,39 @@ const Total = ({ parts }) => {
   );
 };
 
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
 const App = () => {
   const course = {
+    id: 1,
     name: "Half Stack application development",
     parts: [
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
       },
     ],
   };
 
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
+  return <Course course={course} />;
 };
 
 export default App;

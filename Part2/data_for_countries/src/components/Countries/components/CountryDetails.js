8c734c3ffc0cb07d4d35c089f8d65df18cf06139
denis-rim <denis.aurorim@gmail.com>
2021-04-06 15:04:44 +0300

    2.14*: Data for countries, step3

--- Part2/data_for_countries/src/components/Countries/components/CountryDetails.js
+++ Part2/data_for_countries/src/components/Countries/components/CountryDetails.js
@@ -1,31 +1,59 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+const api_key = process.env.REACT_APP_API_KEY;
+const BASE_URL = `http://api.weatherstack.com/current?access_key=${api_key}&units=m&query=`;
 
 const CountryDetails = ({ country }) => {
+  const [weather, setWeather] = useState({});
+
   const { name, capital, population, languages, flag } = country;
+  const { temperature, weather_icons, wind_speed, wind_dir } = weather;
+
+  console.log(weather);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}${name}`).then((response) => {
+      console.log(response.data);
+      setWeather(response.data.current);
+    });
+  }, []);
+
   return (
     <div className="country-detail">
-      <h3>{name}</h3>
-      <div>
-        <p>capital: {capital}</p>
-        <p>population: {population}</p>
+      <div className="country-detail__country">
+        <h3>{name}</h3>
+        <div>
+          <p>capital: {capital}</p>
+          <p>population: {population}</p>
+        </div>
+        <h3>Languages</h3>
+        <div>
+          <ul>
+            {languages.map((language) => {
+              return (
+                <li key={`${name}${language.nativeName}`}>{language.name}</li>
+              );
+            })}
+          </ul>
+        </div>
+        <div>
+          <img
+            style={{ width: "200px", border: "1px solid black" }}
+            src={flag}
+            alt={`${name} flag`}
+          />
+        </div>
       </div>
-      <h3>Languages</h3>
-      <div>
-        <ul>
-          {languages.map((language) => {
-            return (
-              <li key={`${name}${language.nativeName}`}>{language.name}</li>
-            );
-          })}
-        </ul>
+
+      <div className="country-detail__weather">
+        <h3>Weather in {capital}</h3>
+        <img src={weather_icons} />
+        <p>Temperature: {temperature}</p>
+        <p>
+          Wind: {wind_speed} km/h, direction: {wind_dir}
+        </p>
       </div>
-      <div>
-        <img
-          style={{ width: "200px", border: "1px solid black" }}
-          src={flag}
-          alt={`${name} flag`}
-        />
-      </div>{" "}
     </div>
   );
 };
